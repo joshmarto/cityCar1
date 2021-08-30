287a4f5dd6b7a799738ac3a094d1f24c7024fd63
joshmarto <65306069+joshmarto@users.noreply.github.com>
2021-08-30 13:01:03 -0600

    add calcular function

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console.log('Working...');
+
+const submit = document.getElementById('btnSubmit');
+submit.addEventListener('click', calcular);
+
+function calcular() {
+    const service = document.getElementById('service').value;
+    const passengers = document.getElementById('passengers').value;
+    const payment = document.getElementById('cash').checked;
+    
+    console.log(service, passengers, payment);
+}
